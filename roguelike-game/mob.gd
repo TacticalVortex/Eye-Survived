3ce7f20ae8688555a7f8592b05efaefa99b155ec
TacticalVortex <hdude2010@gmail.com>
2025-03-17 21:25:06 -0500

    Changed mob health
    Made it so starting from stage 4, there is a chance that whenever a mob spawns, it requires 2 hits to kill instead of 1. Then, for every stage you pass, the chance of a mob spawning with 2 health increases. Also made the start and quit buttons on the start menu bigger.

--- roguelike-game/mob.gd
+++ roguelike-game/mob.gd
@@ -3,21 +3,29 @@ extends RigidBody2D
 var chest = preload("res://chest.tscn")
 var chest_array = Global.chests
 
+var health = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
 	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
+	if Global.stage > 3:
+		if randi() % 100 < (Global.stage * 2):
+			health = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	pass
 
 func hit():
-	SoundManager.set_volume(1.0)
-	SoundManager.play("res://art/enemy_death.ogg")
-	if randi() % 100 < 6:
-		drop_item()
-	queue_free()
+	if health > 0:
+		health -= 1
+	else:
+		SoundManager.set_volume(1.0)
+		SoundManager.play("res://art/enemy_death.ogg")
+		if randi() % 100 < 6:
+			drop_item()
+		queue_free()
 
 func drop_item():
 	var item = chest.instantiate()
