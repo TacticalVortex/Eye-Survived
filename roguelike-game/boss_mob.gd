0964f403b07ff1d3c25241723b6f0a456bf97d8b
Sonu <156054782+SonuTheNecro@users.noreply.github.com>
2025-04-06 01:30:47 -0500

    refactor: mobs
    Mobs now use OOP class_name Mob
    All Randi -> Randi_ranges
    Added Static typing to all variables where It made sense to do so
    Added Return types whenever it made sense to do so

--- roguelike-game/boss_mob.gd
+++ roguelike-game/boss_mob.gd
@@ -1,42 +1,20 @@
-extends RigidBody2D
+extends Mob
 
-var health
-var size
 
-var chest = preload("res://chest.tscn")
-var chest_array = Global.chests
+@onready var size : float = 1.3 + (Global.stage * 0.05)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
+
+func _ready() -> void:
+	super._ready()
 	health = 20 + (5 * Global.stage)
-	size = 1.3 + (Global.stage * 0.05)
-	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
-	$AnimatedSprite2D.scale = Vector2(size, size)
-	$CollisionShape2D.scale = Vector2(size, size)
+	self.scale = Vector2(size,size)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
-	pass
-
-func hit():
+## Overrides Mob's Hit
+func hit() -> void:
 	if health > 0:
 		health -= 1
 	else:
 		SoundManager.set_volume(2.0)
 		SoundManager.play("res://art/enemy_death.ogg")
 		drop_item()
-		queue_free()
-
-func drop_item():
-	var item = chest.instantiate()
-	item.add_to_group("chest")
-	item.position = position
-	call_deferred("_add_item", item)
-	chest_array.append(item)
-
-func _add_item(item):
-	get_parent().add_child(item)
-
-func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free()
+		call_deferred("queue_free")
