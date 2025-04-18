37fba966c6047d92dcd5c07c974fd0cdb2cc8217
TacticalVortex <hdude2010@gmail.com>
2025-04-17 22:20:21 -0500

    Changed mob sizes
    Made it so if a mob has 2 health, its size is increased so you can tell it has more health.

--- roguelike-game/mob.gd
+++ roguelike-game/mob.gd
@@ -13,6 +13,8 @@ func _ready() -> void:
 	if Global.stage > 3:
 		if randi_range(0,100)  < (Global.stage * 2):
 			health = 1
+			$AnimatedSprite2D.scale = Vector2(1.05, 1.05)
+			$CollisionShape2D.scale = Vector2(1.05, 1.05)
 
 func hit() -> void:
 	if health > 0:
