bf9c9c2b331a8f305ce4eab34e3b633c88eabb8c
Henry <hdude2010@gmail.com>
2025-05-13 00:09:46 -0500

    Added a default button for settings
    Added a default button which returns all settings to their default values.

--- eye-survived/settings.gd
+++ eye-survived/settings.gd
@@ -48,3 +48,21 @@ func _on_music_slider_value_changed(value):
 func _on_brightness_slider_value_changed(value):
 	$BrightnessLabel.text = "Brightness: " + str(int(value * 5))
 	brightness.emit(value)
+
+
+func _on_default_button_pressed():
+	if !Global.fps_visible:
+		fps_button.emit()
+		$FPSLabel.text = "FPS: On"
+	if !Global.highscore_visible:
+		highscore_button.emit()
+		$ScoreLabel.text = "Highscores: On"
+	if !Global.music_on:
+		music_button.emit()
+		$MusicSlider.show()
+	$MusicLabel.text = "Music: " + str(70)
+	music_volume.emit(0.0)
+	$MusicSlider.value = 0.0
+	$BrightnessLabel.text = "Brightness: " + str(5)
+	brightness.emit(1.0)
+	$BrightnessSlider.value = 1.0
