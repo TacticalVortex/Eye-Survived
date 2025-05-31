802446338488a3a239994f15e86925d131107f07
Henry <hdude2010@gmail.com>
2025-05-31 02:15:05 -0500

    Changed some positions
    Centered the starting position.

--- eye-survived/cursor.gd
+++ eye-survived/cursor.gd
@@ -9,10 +9,8 @@ func _ready():
 func _process(delta):
 	if Global.controller_on:
 		var move_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down", 0.5)
-
 		if move_vector.length() > 0:
 			move_vector = move_vector.normalized() * cursor_speed * delta
-
+			
 		var current_mouse_pos = get_viewport().get_mouse_position()
-		
 		get_viewport().warp_mouse(current_mouse_pos + move_vector)
