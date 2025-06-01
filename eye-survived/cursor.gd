12448eda5020b033d04b65bb230661614987032b
Henry <hdude2010@gmail.com>
2025-06-01 01:19:03 -0500

    Worked on controller cursor movement
    I got the controller to move the cursor in all directions, but it is still a big work in progress.

--- eye-survived/cursor.gd
+++ eye-survived/cursor.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 @onready var cursor = $Sprite2D
-var cursor_speed = 600
+var cursor_speed = 400
 
 func _ready():
 	pass
@@ -10,7 +10,20 @@ func _process(delta):
 	if Global.controller_on:
 		var move_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down", 0.5)
 		if move_vector.length() > 0:
-			move_vector = move_vector.normalized() * cursor_speed * delta
-			
-		var current_mouse_pos = get_viewport().get_mouse_position()
-		get_viewport().warp_mouse(current_mouse_pos + move_vector)
+			var move_x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+			var move_y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+
+			move_vector = Vector2(move_x, move_y)
+
+			if move_vector.length() > 0:
+				move_vector = move_vector.normalized()
+
+			move_vector *= cursor_speed * delta * 5
+
+			var current_mouse_pos = get_viewport().get_mouse_position()
+			var new_mouse_pos = current_mouse_pos + move_vector
+
+			new_mouse_pos.x = clamp(new_mouse_pos.x, 0, 1280)
+			new_mouse_pos.y = clamp(new_mouse_pos.y, 0, 720)
+
+			get_viewport().warp_mouse(new_mouse_pos)
