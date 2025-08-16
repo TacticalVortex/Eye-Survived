3f9fdf07d302e699c932ed21c952e3871cb4a90d
Henry <hdude2010@gmail.com>
2025-08-15 22:20:26 -0500

    Changed stats settings
    Made it so the stats screen is only available after having started a run.

--- eye-survived/menu.gd
+++ eye-survived/menu.gd
@@ -7,7 +7,8 @@ signal stats
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if Global.best_stage == 0:
+		$StatsButton.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
